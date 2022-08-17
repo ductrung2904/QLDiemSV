4c8ec7586eb2bed60b0eb5a4321bda1634c70d2f
Kiwi-7 <60024260+Kiwi-7@users.noreply.github.com>
2022-08-17 22:39:35 +0700

    Update file thong ke, cap nhat them chuc nang xuat excel

--- QuanLyDiemSV/frmThongKeDiem.Designer.cs
+++ QuanLyDiemSV/frmThongKeDiem.Designer.cs
@@ -29,6 +29,7 @@ namespace QuanLyDiemSV
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.cboLopHP = new System.Windows.Forms.ComboBox();
             this.label4 = new System.Windows.Forms.Label();
@@ -37,13 +38,26 @@ namespace QuanLyDiemSV
             this.cboNganhHoc = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.dgvThongKeDiemSV = new System.Windows.Forms.DataGridView();
+            this.M = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.btnRefresh = new System.Windows.Forms.Button();
             this.btnXuatExcel = new System.Windows.Forms.Button();
             this.btnThongKe = new System.Windows.Forms.Button();
             this.btnThoat = new System.Windows.Forms.Button();
+            this.errNH = new System.Windows.Forms.ErrorProvider(this.components);
+            this.saveFileDialogExcel = new System.Windows.Forms.SaveFileDialog();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvThongKeDiemSV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errNH)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -86,6 +100,8 @@ namespace QuanLyDiemSV
             this.cboMonHoc.Name = "cboMonHoc";
             this.cboMonHoc.Size = new System.Drawing.Size(246, 24);
             this.cboMonHoc.TabIndex = 3;
+            this.cboMonHoc.SelectedValueChanged += new System.EventHandler(this.cboMonHoc_SelectedValueChanged);
+            this.cboMonHoc.Click += new System.EventHandler(this.cboMonHoc_Click);
             // 
             // label3
             // 
@@ -103,6 +119,8 @@ namespace QuanLyDiemSV
             this.cboNganhHoc.Name = "cboNganhHoc";
             this.cboNganhHoc.Size = new System.Drawing.Size(246, 24);
             this.cboNganhHoc.TabIndex = 1;
+            this.cboNganhHoc.SelectedValueChanged += new System.EventHandler(this.cboNganhHoc_SelectedValueChanged);
+            this.cboNganhHoc.Click += new System.EventHandler(this.cboNganhHoc_Click);
             // 
             // label2
             // 
@@ -115,11 +133,93 @@ namespace QuanLyDiemSV
             // 
             // dgvThongKeDiemSV
             // 
+            this.dgvThongKeDiemSV.AllowUserToAddRows = false;
             this.dgvThongKeDiemSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThongKeDiemSV.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.M,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column1,
+            this.Column6,
+            this.Column7,
+            this.Column8,
+            this.Column9});
             this.dgvThongKeDiemSV.Location = new System.Drawing.Point(12, 100);
             this.dgvThongKeDiemSV.Name = "dgvThongKeDiemSV";
             this.dgvThongKeDiemSV.Size = new System.Drawing.Size(660, 436);
             this.dgvThongKeDiemSV.TabIndex = 45;
+            // 
+            // M
+            // 
+            this.M.DataPropertyName = "MaNganh";
+            this.M.HeaderText = "Mã Ngành";
+            this.M.Name = "M";
+            this.M.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.DataPropertyName = "TenNganh";
+            this.Column2.HeaderText = "Tên Ngành";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.DataPropertyName = "MaSV";
+            this.Column3.HeaderText = "Mã Sinh Viên";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.DataPropertyName = "TenSV";
+            this.Column4.HeaderText = "Tên Sinh Viên";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.DataPropertyName = "TenMH";
+            this.Column5.HeaderText = "Tên Môn Học";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            this.Column1.DataPropertyName = "MaHocPhan";
+            this.Column1.HeaderText = "Mã Học Phần";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.DataPropertyName = "DiemLT";
+            this.Column6.HeaderText = "Điểm Lý Thuyết";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            this.Column7.DataPropertyName = "DiemTH";
+            this.Column7.HeaderText = "Điểm Thực Hành";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            this.Column8.DataPropertyName = "DiemTB";
+            this.Column8.HeaderText = "Điểm Trung Bình";
+            this.Column8.Name = "Column8";
+            this.Column8.ReadOnly = true;
+            // 
+            // Column9
+            // 
+            this.Column9.DataPropertyName = "DiemH4";
+            this.Column9.HeaderText = "Điểm Hệ 4";
+            this.Column9.Name = "Column9";
+            this.Column9.ReadOnly = true;
             // 
             // label1
             // 
@@ -142,6 +242,7 @@ namespace QuanLyDiemSV
             this.btnRefresh.TabIndex = 49;
             this.btnRefresh.Text = "Refresh";
             this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
             // 
             // btnXuatExcel
             // 
@@ -153,6 +254,7 @@ namespace QuanLyDiemSV
             this.btnXuatExcel.TabIndex = 48;
             this.btnXuatExcel.Text = "Xuất Excel";
             this.btnXuatExcel.UseVisualStyleBackColor = true;
+            this.btnXuatExcel.Click += new System.EventHandler(this.btnXuatExcel_Click);
             // 
             // btnThongKe
             // 
@@ -164,6 +266,7 @@ namespace QuanLyDiemSV
             this.btnThongKe.TabIndex = 47;
             this.btnThongKe.Text = "Thống Kê";
             this.btnThongKe.UseVisualStyleBackColor = true;
+            this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
             // 
             // btnThoat
             // 
@@ -175,6 +278,16 @@ namespace QuanLyDiemSV
             this.btnThoat.TabIndex = 44;
             this.btnThoat.Text = "Thoát";
             this.btnThoat.UseVisualStyleBackColor = true;
+            this.btnThoat.Click += new System.EventHandler(this.btnThoat_Click);
+            // 
+            // errNH
+            // 
+            this.errNH.ContainerControl = this;
+            // 
+            // saveFileDialogExcel
+            // 
+            this.saveFileDialogExcel.Filter = "Excel files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            this.saveFileDialogExcel.FilterIndex = 2;
             // 
             // frmThongKeDiem
             // 
@@ -192,9 +305,11 @@ namespace QuanLyDiemSV
             this.MinimumSize = new System.Drawing.Size(1086, 602);
             this.Name = "frmThongKeDiem";
             this.Text = "Thống kê điểm sinh viên";
+            this.Load += new System.EventHandler(this.frmThongKeDiem_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgvThongKeDiemSV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errNH)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,5 +330,17 @@ namespace QuanLyDiemSV
         private System.Windows.Forms.Button btnXuatExcel;
         private System.Windows.Forms.Button btnThongKe;
         private System.Windows.Forms.Button btnThoat;
+        private System.Windows.Forms.ErrorProvider errNH;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogExcel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn M;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
     }
 }
--- QuanLyDiemSV/frmThongKeDiem.cs
+++ QuanLyDiemSV/frmThongKeDiem.cs
@@ -8,13 +8,230 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
 namespace QuanLyDiemSV
 {
     public partial class frmThongKeDiem : Form
     {
+        QLDiemSVDataContext db = new QLDiemSVDataContext();
+
         public frmThongKeDiem()
         {
             InitializeComponent();
         }
+        void loadData()
+        {
+            cboMonHoc.Enabled = false;
+            cboLopHP.Enabled = false;
+        }
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+            if (cboNganhHoc.GetItemText(cboNganhHoc.SelectedItem) == "")
+            {
+                errNH.SetError(cboNganhHoc, "Bạn chưa chọn Ngành học!");
+            }
+            else if (cboMonHoc.Text == "")
+            {
+                var query = from diem in db.Diems
+                            join sv in db.SinhViens on diem.MaSV equals sv.MaSV
+                            join nh in db.NganhHocs on sv.MaNganh equals nh.MaNganh
+                            join lop in db.Lops on diem.MaLop equals lop.MaLop
+                            join mh in db.MonHocs on lop.MaMH equals mh.MaMH                         
+                            where   nh.MaNganh == mh.MaNganh &&
+                                    nh.MaNganh == sv.MaNganh &&
+                                    sv.MaSV == diem.MaSV &&
+                                    mh.MaMH == lop.MaMH &&
+                                    nh.MaNganh.ToString() == cboNganhHoc.GetItemText(cboNganhHoc.SelectedItem)
+                            select new
+                            {
+                                MaSV = sv.MaSV,
+                                TenSV = sv.TenSV,
+                                MaNganh = nh.MaNganh,
+                                TenNganh = nh.TenNganh,
+                                TenMH = mh.TenMH,
+                                MaHocPhan = lop.MaHocPhan,
+                                DiemLT = Math.Round(double.Parse(diem.DiemLT.ToString()), 2),
+                                DiemTH = Math.Round(double.Parse(diem.DiemTH.ToString()), 2),
+                                DiemTB = Math.Round(double.Parse(diem.DiemTB.ToString()), 2),
+                                DiemH4 = Math.Round(double.Parse(diem.DiemHe4.ToString()), 2),
+                            };
+
+                dgvThongKeDiemSV.DataSource = query;
+            }
+            else if (cboLopHP.Text == "")
+            {
+                var query = from diem in db.Diems
+                            join sv in db.SinhViens on diem.MaSV equals sv.MaSV
+                            join nh in db.NganhHocs on sv.MaNganh equals nh.MaNganh
+                            join lop in db.Lops on diem.MaLop equals lop.MaLop
+                            join mh in db.MonHocs on lop.MaMH equals mh.MaMH
+                            where nh.MaNganh == mh.MaNganh &&
+                                    nh.MaNganh == sv.MaNganh &&
+                                    sv.MaSV == diem.MaSV &&
+                                    mh.MaMH == lop.MaMH &&
+                                    nh.MaNganh.ToString() == this.cboNganhHoc.GetItemText(this.cboNganhHoc.SelectedItem) &&
+                                    mh.MaMH.ToString() == this.cboMonHoc.GetItemText(this.cboMonHoc.SelectedItem)
+                            select new
+                            {
+                                MaSV = sv.MaSV,
+                                TenSV = sv.TenSV,
+                                MaNganh = nh.MaNganh,
+                                TenNganh = nh.TenNganh,
+                                TenMH = mh.TenMH,
+                                MaHocPhan = lop.MaHocPhan,
+                                DiemLT = Math.Round(double.Parse(diem.DiemLT.ToString()), 2),
+                                DiemTH = Math.Round(double.Parse(diem.DiemTH.ToString()), 2),
+                                DiemTB = Math.Round(double.Parse(diem.DiemTB.ToString()), 2),
+                                DiemH4 = Math.Round(double.Parse(diem.DiemHe4.ToString()), 2),
+                            };
+
+                dgvThongKeDiemSV.DataSource = query;
+            }
+            else
+            {
+                var query = from diem in db.Diems
+                            join sv in db.SinhViens on diem.MaSV equals sv.MaSV
+                            join nh in db.NganhHocs on sv.MaNganh equals nh.MaNganh
+                            join lop in db.Lops on diem.MaLop equals lop.MaLop
+                            join mh in db.MonHocs on lop.MaMH equals mh.MaMH
+                            where nh.MaNganh == mh.MaNganh &&
+                                    nh.MaNganh == sv.MaNganh &&
+                                    sv.MaSV == diem.MaSV &&
+                                    mh.MaMH == lop.MaMH &&
+                                    nh.MaNganh.ToString() == this.cboNganhHoc.GetItemText(this.cboNganhHoc.SelectedItem) &&
+                                    mh.MaMH.ToString() == this.cboMonHoc.GetItemText(this.cboMonHoc.SelectedItem) &&
+                                    lop.MaHocPhan.ToString() == this.cboLopHP.GetItemText(this.cboLopHP.SelectedItem)
+                            select new
+                            {
+                                MaSV = sv.MaSV,
+                                TenSV = sv.TenSV,
+                                MaNganh = nh.MaNganh,
+                                TenNganh = nh.TenNganh,
+                                TenMH = mh.TenMH,
+                                MaHocPhan = lop.MaHocPhan,
+                                DiemLT = Math.Round(double.Parse(diem.DiemLT.ToString()), 2),
+                                DiemTH = Math.Round(double.Parse(diem.DiemTH.ToString()), 2),
+                                DiemTB = Math.Round(double.Parse(diem.DiemTB.ToString()), 2),
+                                DiemH4 = Math.Round(double.Parse(diem.DiemHe4.ToString()), 2),
+                            };
+
+                dgvThongKeDiemSV.DataSource = query;
+            }
+        }
+
+        private void btnXuatExcel_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialogExcel.ShowDialog() == DialogResult.OK)
+            {
+                toExcel(dgvThongKeDiemSV, saveFileDialogExcel.FileName);
+            }
+        }
+
+        private void toExcel(DataGridView dgvThongKeDiemSV, string fileName)
+        {
+            Microsoft.Office.Interop.Excel.Application excel;
+            Microsoft.Office.Interop.Excel.Workbook workbook;
+            Microsoft.Office.Interop.Excel.Worksheet worksheet;
+
+            try
+            {
+                excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+
+                workbook = excel.Workbooks.Add(Type.Missing);
+                worksheet = (Microsoft.Office.Interop.Excel.Worksheet)workbook.Sheets["Sheet1"];
+                worksheet.Name = "BANG THONG KE SINH VIEN";
+
+                for (int i = 0; i < dgvThongKeDiemSV.ColumnCount; i++)
+                {
+                    worksheet.Cells[1, 1 + i] = dgvThongKeDiemSV.Columns[i].HeaderText;
+                    
+                }
+
+                for (int i = 0; i < dgvThongKeDiemSV.RowCount; i++)
+                {
+                    for (int y = 0; y < dgvThongKeDiemSV.ColumnCount; y++)
+                    {
+                        worksheet.Cells[i + 2, y + 1] = dgvThongKeDiemSV.Rows[i].Cells[y].Value.ToString();
+                    }
+                    worksheet.Columns[i + 1].ColumnWidth = 15;
+                }
+
+                workbook.SaveAs(fileName);
+                workbook.Close();
+                excel.Quit();
+                MessageBox.Show("Xuất dữ liệu thành công.!");
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+            finally
+            {
+                workbook = null;
+                worksheet = null;
+            }
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            cboLopHP.SelectedValue = "";
+            cboMonHoc.SelectedValue = "";
+            cboNganhHoc.SelectedValue = "";
+
+            dgvThongKeDiemSV.DataSource = null;
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmThongKeDiem_Load(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void cboNganhHoc_SelectedValueChanged(object sender, EventArgs e)
+        {
+            var mh_Theo_NganhHoc = db.MonHocs.
+                Where(x => x.MaNganh == this.cboNganhHoc.GetItemText(this.cboNganhHoc.SelectedItem)).ToList();
+
+            cboMonHoc.DataSource = mh_Theo_NganhHoc;
+            cboMonHoc.ValueMember = "MaMH";
+            cboMonHoc.DisplayMember = "MaMH";
+
+            cboMonHoc.Text = "";
+            cboLopHP.Text = "";
+        }
+
+        private void cboMonHoc_SelectedValueChanged(object sender, EventArgs e)
+        {
+            var lh_Theo_MonHoc = db.Lops.
+                Where(x => x.MaMH == this.cboMonHoc.GetItemText(this.cboMonHoc.SelectedItem)).ToList();
+
+            cboLopHP.DataSource = lh_Theo_MonHoc;
+            cboLopHP.ValueMember = "MaHocPhan";
+            cboLopHP.DisplayMember = "MaHocPhan";
+
+            cboLopHP.Text = "";
+        }
+
+        private void cboNganhHoc_Click(object sender, EventArgs e)
+        {
+            cboMonHoc.Enabled = true;
+            errNH.Clear();
+
+            cboNganhHoc.DataSource = db.NganhHocs.ToList();
+            cboNganhHoc.ValueMember = "MaNganh";
+            cboNganhHoc.DisplayMember = "MaNganh";
+        }
+
+        private void cboMonHoc_Click(object sender, EventArgs e)
+        {
+            cboLopHP.Enabled = true;
+        }
     }
 }
+
